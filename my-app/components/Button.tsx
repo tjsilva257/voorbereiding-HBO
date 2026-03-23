00165fb9803c6b8f97694d592ca6b99431f7b50a
Dennis Xu <302056276@student.rocmondriaan.nl>
2026-03-23 14:49:27 +0100

    added components and a start up page

---
+++ my-app/components/Button.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Button() {
+    return(
+        <div>
+            <button className="bg-red-300 px-5 py-2 rounded-lg hover:size-30 transition duration-75 ease-in-out text-4xl font-medium">Start</button>
+        </div>
+    )
+}
---
+++ my-app/components/Card.tsx
@@ -0,0 +1,9 @@
+export function Card() {
+  return (
+    <div>
+        <div className="bg-green-200 p-2 rounded-lg w-1/3 h-30">
+            hi
+        </div>
+    </div>
+  );
+}
