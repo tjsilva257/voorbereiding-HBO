aabebc4569c9b1ae60657d88023ee9587b291166
Dennis Xu <302056276@student.rocmondriaan.nl>
2026-03-23 15:31:17 +0100

    updated the page and button with some gradient color

--- my-app/components/Button.tsx
+++ my-app/components/Button.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import Link from 'next/link'
+
 export default function Button() {
     return(
         <div>
-            <button className="bg-red-300 px-5 py-2 rounded-lg hover:size-30 transition duration-75 ease-in-out text-4xl font-medium">Start</button>
+            <button className="bg-linear-to-bl from-violet-500 to-fuchsia-500 text-4xl font-medium px-7 py-3 rounded-lg ease-in-out hover:scale-110 focus:outline-2 focus:outline-offset-2 focus:outline-voilet-400 active:bg-gray-400 transition duration-500">
+                <Link href="">Start</Link>
+            </button>
         </div>
     )
 }
